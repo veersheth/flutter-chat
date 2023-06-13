85490169a9848176536bee41364a1a4dc1dd399f
Veer258 <vtube258@gmail.com>
2023-06-13 16:23:08 +0530

    Add ability to signout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/helper/helper_function.dart';
+import 'package:flutter_chat_app/pages/login_page.dart';
+import 'package:flutter_chat_app/service/auth_service.dart';
+import 'package:flutter_chat_app/widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +12,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  AuthService authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home Page"),
+        child: ElevatedButton(
+          child: Text("SIGN OUT"),
+          onPressed: () {
+            print("Signing out");
+            authService.signout();
+            print("Signout successful");
+            // nextScreen(context, const LoginPage())
+          },
+        ),
       ),
     );
   }
--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_chat_app/helper/helper_function.dart';
 import 'package:flutter_chat_app/service/database_service.dart';
 
 class AuthService {
@@ -24,4 +25,15 @@ class AuthService {
   }
 
   //sign out
+
+  Future signout() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
 }

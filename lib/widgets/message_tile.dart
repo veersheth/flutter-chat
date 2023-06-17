0f61840ecf8f2caf61f85da83cb06948c17a1703
Veer258 <vtube258@gmail.com>
2023-06-17 18:13:09 +0530

    chat feature works as expected

--- lib/widgets/message_tile.dart
+++ lib/widgets/message_tile.dart
@@ -20,19 +20,25 @@ class _MessageTileState extends State<MessageTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      margin: widget.sentByMe
+          ? const EdgeInsets.only(top: 4, bottom: 4, left: 120, right: 10)
+          : const EdgeInsets.only(top: 4, bottom: 4, right: 120, left: 10),
+      alignment: Alignment.centerLeft,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: widget.sentByMe
             ? Theme.of(context).colorScheme.primaryContainer
-            : Theme.of(context).colorScheme.secondaryContainer,
+            : Theme.of(context).colorScheme.onInverseSurface,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(widget.sender),
-          Text(widget.message),
+          widget.sentByMe ? const SizedBox(height: 0) : Text(widget.sender),
+          Text(
+            widget.message,
+            softWrap: true,
+          ),
         ],
       ),
     );
